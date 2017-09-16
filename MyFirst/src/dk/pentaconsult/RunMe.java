35abc1dc5ebb9024864d982b082ddd0dbb75f0f8
kim <kim@localhost>
2017-09-17 00:17:56 +0200

    EN tester

---
+++ MyFirst/src/dk/pentaconsult/RunMe.java
@@ -0,0 +1,14 @@
+package dk.pentaconsult;
+
+public class RunMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Howdy...");
+		
+		System.out.println("Howdy...2");
+
+	}
+
+}
