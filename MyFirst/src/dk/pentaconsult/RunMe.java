188601170ba7f284d54d725e3d24251f3ee4c346
kim <kim@localhost>
2017-09-17 00:36:08 +0200

    Commit nr 2

---
+++ MyFirst/src/dk/pentaconsult/RunMe.java
@@ -0,0 +1,20 @@
+package dk.pentaconsult;
+
+public class RunMe {
+	
+	static int i=0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Howdy..."+i);
+		
+		i++;
+		System.out.println("Howdy...2" + i);
+		
+		System.out.println("Hello world...");
+		
+
+	}
+
+}
